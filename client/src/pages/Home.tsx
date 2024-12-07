cb4d0cd8a85d7d70cbadbf9a1559d42f8b3ffb6d
Pranav Tartey <pranavtartey@gmail.com>
2024-12-07 12:46:45 +0530

    added the router and shadcn

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>This is the home page</p>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
